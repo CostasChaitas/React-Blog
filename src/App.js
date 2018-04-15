c5f648924005f06fa3e98b8b111105772e942af0
CostasChaitas <xaitakon@gmail.com>
2018-04-15 19:35:38 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Blog from './containers/Blog/Blog';
+
+class App extends Component {
+  render () {
+    return (
+
+      // in case app default path is not / declare it here <BrowserRouter basename="/my-app">
+
+      <BrowserRouter>
+        <div className="App">
+          <Blog />
+        </div>
+      </BrowserRouter>
+
+
+
+    );
+  }
+}
+
+export default App;
